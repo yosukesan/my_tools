47aee96f12c14bfb7ef02c329b2502ceb7e75573
yosuke <crescent_with_fullmoon@hotmail.com>
2020-01-07 00:48:06 +0900

    add thinkpad x250 config

--- xrandr/dual_display.sh
+++ xrandr/dual_display.sh
@@ -1,1 +1,6 @@
-xrandr --output LVDS-1 --auto --output HDMI-1 --auto --right-of LVDS-1
+
+# Asus x200
+#xrandr --output LVDS-1 --auto --output HDMI-1 --auto --right-of LVDS-1
+
+# thinkpad X250
+xrandr --output eDP-1 --auto --output DP-2 --auto --right-of eDP-1
